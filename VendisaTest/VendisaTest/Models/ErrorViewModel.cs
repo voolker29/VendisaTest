fe67cdb4c2b53f9028f7b572fd6577909b8fefdc
Ardashirov Vladimir <vladimir-ardashirov@mail.ru>
2023-11-09 14:50:33 +0300

    recreate project

---
+++ VendisaTest/VendisaTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendisaTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
